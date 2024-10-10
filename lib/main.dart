64afbc03a35dfcbd1c4f633921e7e0e9c6bb270d
jgalicia-dev <javiergaliciaglz@gmail.com>
2024-10-10 17:16:23 -0600

    Changing to CheckboxListTile.

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,81 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for custom labeled checkbox.
+void main() => runApp(const CheckboxListTileApp());
 
-void main() => runApp(const LabeledCheckboxApp());
-
-class LabeledCheckboxApp extends StatelessWidget {
-  const LabeledCheckboxApp({super.key});
+class CheckboxListTileApp extends StatelessWidget {
+  const CheckboxListTileApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
-      home: const LabeledCheckboxExample(),
+      home: const CheckboxListTileExample(),
     );
   }
 }
 
-class LabeledCheckbox extends StatelessWidget {
-  const LabeledCheckbox({
-    super.key,
-    required this.label,
-    required this.padding,
-    required this.value,
-    required this.onChanged,
-  });
-
-  final String label;
-  final EdgeInsets padding;
-  final bool value;
-  final ValueChanged<bool> onChanged;
+class CheckboxListTileExample extends StatefulWidget {
+  const CheckboxListTileExample({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        onChanged(!value);
-      },
-      child: Padding(
-        padding: padding,
-        child: Row(
-          children: <Widget>[
-            Expanded(
-                child: Column(
-              children: [
-                Text(label),
-              ],
-            )),
-            Checkbox(
-              value: value,
-              onChanged: (bool? newValue) {
-                onChanged(newValue!);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  State<CheckboxListTileExample> createState() =>
+      _CheckboxListTileExampleState();
 }
 
-class LabeledCheckboxExample extends StatefulWidget {
-  const LabeledCheckboxExample({super.key});
+class _CheckboxListTileExampleState extends State<CheckboxListTileExample> {
+  bool checkboxValue1 = false;
+  bool checkboxValue2 = true;
+  bool checkboxValue3 = true;
 
-  @override
-  State<LabeledCheckboxExample> createState() => _LabeledCheckboxExampleState();
-}
-
-class _LabeledCheckboxExampleState extends State<LabeledCheckboxExample> {
-  bool _isSelected = false;
-  bool _isSelectedos = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Custom Labeled Checkbox Sample')),
-      body: Center(
-        child: Column(
-          children: [
-            LabeledCheckbox(
-              label: 'This is the label text',
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              value: _isSelected,
-              onChanged: (bool newValue) {
-                setState(() {
-                  _isSelected = newValue;
-                });
-              },
+      appBar: AppBar(title: const Text('CheckboxListTile Sample')),
+      body: Column(
+        children: <Widget>[
+          CheckboxListTile(
+            value: checkboxValue1,
+            onChanged: (bool? value) {
+              setState(() {
+                checkboxValue1 = value!;
+              });
+            },
+            title: const ExpansionTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              title: Text('Header'),
+              children: [
+                Text('sdsadjsad'),
+              ],
             ),
-            LabeledCheckbox(
-              label: 'This is the label text',
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              value: _isSelectedos,
-              onChanged: (bool newValue) {
-                setState(() {
-                  _isSelectedos = newValue;
-                });
-              },
-            ),
-          ],
-        ),
+            //subtitle: const Text('Supporting text'),
+          ),
+          const Divider(height: 0),
+          CheckboxListTile(
+            value: checkboxValue2,
+            onChanged: (bool? value) {
+              setState(() {
+                checkboxValue2 = value!;
+              });
+            },
+            title: const Text('Headline'),
+            subtitle: const Text(
+                'Longer supporting text to demonstrate how the text wraps and the checkbox is centered vertically with the text.'),
+          ),
+          const Divider(height: 0),
+          CheckboxListTile(
+            value: checkboxValue3,
+            onChanged: (bool? value) {
+              setState(() {
+                checkboxValue3 = value!;
+              });
+            },
+            title: const Text('Headline'),
+            subtitle: const Text(
+                "Longer supporting text to demonstrate how the text wraps and how setting 'CheckboxListTile.isThreeLine = true' aligns the checkbox to the top vertically with the text."),
+            isThreeLine: true,
+          ),
+          const Divider(height: 0),
+        ],
       ),
     );
   }
